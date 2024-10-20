faa8153fbdb1c9db116ff67f98ec7d30bfcb91b7
OmkarSatav <omkar7v2@gmail.com>
2024-10-20 22:52:19 +0530

    Code Before Removing Comments

--- MiniProject/recordtypes/employee.h
+++ MiniProject/recordtypes/employee.h
@@ -6,12 +6,10 @@ struct Employee {
     char name[25]; // Name of the employee
     char gender; // M -> Male, F -> Female, O -> Other
     int age; // Age of the employee
-
-    // Login Credentials
     char login[30]; // Format: name-id (name will be the first word in the structure member `name`)
     char password[30]; // Password for the employee
 
-    int employeeType; // 0 for Administrator, 1 for Manager, 2 for Bank Employee
+    int employeeType; //1 for Manager, 2 for Bank Employee
 };
 
 #endif // EMPLOYEE_RECORD
